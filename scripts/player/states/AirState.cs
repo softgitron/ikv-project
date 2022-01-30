07cd88484c623433acea14cdae72d3c379ff0ddd
EddieTheCubeHead <eetu.asikainen@student.lut.fi>
2022-01-30 12:36:47 +0200

    File endings?

--- scripts/player/states/AirState.cs
+++ scripts/player/states/AirState.cs
@@ -3,50 +3,50 @@ using Godot;
 
 public class AirState : PlayerState
 {
-    public PlayerState ProcessState(Player player)
-    {
-        int direction = Math.Sign(Input.GetActionStrength("right_button") - Input.GetActionStrength("left_button"));
-        if (direction > 0)
-        {
-            player.sprite.FlipH = false;
-        }
-        else if (direction < 0)
-        {
-            player.sprite.FlipH = true;
-        }
-        player.AirMove(direction);
-        if (Input.IsActionJustPressed("interact_button"))
-        {
-            player.HandleInteract();
-        }
-        if (Input.IsActionJustPressed("jump_button"))
-        {
-            if (player.doubleJump > 0)
-            {
-                player.velocity.y = -player.jump;
-                player.sprite.Stop();
-                player.sprite.Play("jump");
-                player.doubleJump--;
-            }
-        }
+	public PlayerState ProcessState(Player player)
+	{
+		int direction = Math.Sign(Input.GetActionStrength("right_button") - Input.GetActionStrength("left_button"));
+		if (direction > 0)
+		{
+			player.sprite.FlipH = false;
+		}
+		else if (direction < 0)
+		{
+			player.sprite.FlipH = true;
+		}
+		player.AirMove(direction);
+		if (Input.IsActionJustPressed("interact_button"))
+		{
+			player.HandleInteract();
+		}
+		if (Input.IsActionJustPressed("jump_button"))
+		{
+			if (player.doubleJump > 0)
+			{
+				player.velocity.y = -player.jump;
+				player.sprite.Stop();
+				player.sprite.Play("jump");
+				player.doubleJump--;
+			}
+		}
 
-        return null;
-    }
-    public void HandleAnimationFinished(Player player)
-    {
-        player.sprite.Play("idle"); // Fall animation here
-    }
+		return null;
+	}
+	public void HandleAnimationFinished(Player player)
+	{
+		player.sprite.Play("idle"); // Fall animation here
+	}
 
-    public void ExitState(Player player)
-    {
-        if (Main.player == 2)   // If player is light
-        {
-            player.doubleJump = 1;
-        }
-    }
-    public void EnterState(Player player)
-    {
-        player.sprite.Stop();
-        player.sprite.Play("jump");
-    }
+	public void ExitState(Player player)
+	{
+		if (Main.player == 2)   // If player is light
+		{
+			player.doubleJump = 1;
+		}
+	}
+	public void EnterState(Player player)
+	{
+		player.sprite.Stop();
+		player.sprite.Play("jump");
+	}
 }
--- scripts/player/states/IdleState.cs
+++ scripts/player/states/IdleState.cs
@@ -2,48 +2,48 @@ using Godot;
 
 public class IdleState : PlayerState
 {
-    public void HandleAnimationFinished(Player player)
-    {
-        player.sprite.Play("idle");
-    }
-    public void ExitState(Player player)
-    {
+	public void HandleAnimationFinished(Player player)
+	{
+		player.sprite.Play("idle");
+	}
+	public void ExitState(Player player)
+	{
 
-    }
-    public void EnterState(Player player)
-    {
-        player.sprite.Play("idle");
-    }
-    public PlayerState ProcessState(Player player)
-    {
-        if (Input.IsActionJustPressed("left_button") || Input.IsActionJustPressed("right_button"))
-        {
-            return new MoveState();
-        }
+	}
+	public void EnterState(Player player)
+	{
+		player.sprite.Play("idle");
+	}
+	public PlayerState ProcessState(Player player)
+	{
+		if (Input.IsActionJustPressed("left_button") || Input.IsActionJustPressed("right_button"))
+		{
+			return new MoveState();
+		}
 
-        if (Input.IsActionJustPressed("jump_button"))
-        {
-            player.velocity.y -= player.jump;
-            return new MoveState();
-        }
+		if (Input.IsActionJustPressed("jump_button"))
+		{
+			player.velocity.y -= player.jump;
+			return new MoveState();
+		}
 
-        if (Input.IsActionJustPressed("interact_button"))
-        {
-            player.HandleInteract();
-        }
+		if (Input.IsActionJustPressed("interact_button"))
+		{
+			player.HandleInteract();
+		}
 
-        if (Input.IsActionJustPressed("crouch_button"))
-        {
-            player.Drop();
-        }
+		if (Input.IsActionJustPressed("crouch_button"))
+		{
+			player.Drop();
+		}
 
-        player.GroundMove(0);
+		player.GroundMove(0);
 
-        if (player.velocity.y != 0)
-        {
-            return new AirState();
-        }
+		if (player.velocity.y != 0)
+		{
+			return new AirState();
+		}
 
-        return null;
-    }
+		return null;
+	}
 }
