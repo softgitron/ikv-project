01906363d0fe1e4c0068575c113272677afcbe2f
Tompparella <58560424+Tompparella@users.noreply.github.com>
2022-01-30 15:21:42 +0200

    Final shit

--- scripts/assets/Hittable.cs
+++ scripts/assets/Hittable.cs
@@ -3,14 +3,14 @@ using System;
 
 public class Hittable : StaticBody2D
 {
-    public void Hit()
-    {
-        Node2D parent = (Node2D)GetParent();
-        parent.Scale = parent.Scale + new Vector2(0, -0.25f);
-        parent.Position = parent.Position + new Vector2(0, 50);
-        if (parent.Scale.y < 0.5)
-        {
-            parent.QueueFree();
-        }
-    }
+	public void Hit()
+	{
+		Node2D parent = (Node2D)GetParent();
+		parent.Scale = parent.Scale + new Vector2(0, -0.25f);
+		parent.Position = parent.Position + new Vector2(0, 50);
+		if (parent.Scale.y < 0.5)
+		{
+			parent.QueueFree();
+		}
+	}
 }
