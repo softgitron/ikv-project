3841739227ab0e936b1503e342cdd349515b77be
EddieTheCubeHead <eetu.asikainen@student.lut.fi>
2022-01-30 12:12:32 +0200

    Player theme sync

--- scripts/audio/PlayerThemeSync.cs
+++ scripts/audio/PlayerThemeSync.cs
@@ -1,23 +1,31 @@
-namespace DefaultNamespace;
+using System;
 
 public class PlayerThemeSync : StateSync<Boolean>
 {
-    private string themeName;
-    private AudioSource audioSource;
+	private string themeName;
+	private AudioSource audioSource;
+	public StateSyncImpl<Boolean> stateSync;
 
-    public PlayerThemeSync(AudioSource audioSource, string themeName)
-    {
-        this.themeName = themeName;
-        this.audioSource = audioSource;
-    }
-    public void StateTrigger(Boolean newState) {
-        if (newState)
-        {
-            this.audioSource.FadeInTrack(this.themeName);
-        }
-        else
-        {
-            this.audioSource.FadeOutTrack(this.themeName);
-        }
-    }
+	public PlayerThemeSync(AudioSource audioSource, string themeName)
+	{
+		this.themeName = themeName;
+		this.audioSource = audioSource;
+		stateSync = new StateSyncImpl<Boolean>(this, false, "ThemeSync");
+	}
+
+	public void SetThemeState(bool state)
+	{
+		this.stateSync.SetState(state);
+	}
+	
+	public void StateTrigger(Boolean newState) {
+		if (newState)
+		{
+			this.audioSource.FadeInTrack(this.themeName);
+		}
+		else
+		{
+			this.audioSource.FadeOutTrack(this.themeName);
+		}
+	}
 }
