1d59b34fd673f16c29ab81933b56bc8d935680cc
EddieTheCubeHead <eetu.asikainen@student.lut.fi>
2022-01-30 09:00:09 +0200

    Refactor player state to use strategy pattern

--- scripts/player/Player.cs
+++ scripts/player/Player.cs
@@ -5,31 +5,24 @@ using System.Collections.Generic;
 
 public class Player : KinematicBody2D
 {
-    private Vector2 velocity = Vector2.Zero;
+    public Vector2 velocity = Vector2.Zero;
     [Export]
     private int maxSpeed = 100;
     [Export]
-    private int jump = 1500;
+    public int jump = 1500;
     [Export]
     private float acceleration = 100;
     [Export]
     private int gravity = 100;
-    private Sprite sprite;
+    public Sprite sprite;
     public List<Area2D> surroundingInteractives = new List<Area2D>();
     private Item pickedItem;
     public int ladders = 0;
-    private string type = "light";
-    private int doubleJump = 0;
+    public string type = "light";
+    public int doubleJump = 0;
     private int attack = 0;
-
-    private string[] states =
-    {
-        "idle",
-        "move",
-        "air"
-    };
-    private List<string> stateStack = new List<string>();
-    private string currentState = "idle";
+    private List<PlayerState> stateStack = new List<PlayerState>();
+    private PlayerState currentState = new IdleState();
 
     public override void _Ready()
     {
@@ -54,20 +47,10 @@ public class Player : KinematicBody2D
     public override void _PhysicsProcess(float delta)
     {
 
-        switch (currentState)
+        PlayerState newState = currentState.ProcessState(this);
+        if (newState != null)
         {
-            case "idle":
-                HandleIdleState();
-                break;
-            case "move":
-                HandleMoveState();
-                break;
-            case "air":
-                HandleAirState();
-                break;
-            default:
-                GD.Print("Kyrpä");
-                break;
+            ChangeState(newState);
         }
         GD.Print(currentState);
 
@@ -77,112 +60,24 @@ public class Player : KinematicBody2D
         }
     }
 
-    private void GroundMove(int direction)
+    public void GroundMove(int direction)
     {
         velocity.y += gravity;
         velocity.x = maxSpeed * direction;
         velocity = MoveAndSlide(velocity);
     }
-    private void AirMove(int direction)
+    public void AirMove(int direction)
     {
         velocity.y += gravity;
         velocity.x = maxSpeed * direction;
         velocity = MoveAndSlide(velocity, Vector2.Up);
         if (IsOnFloor())
         {
-            ChangeState("move");
+            ChangeState(new MoveState());
         }
     }
 
-    private void HandleIdleState()
-    {
-        if (Input.IsActionJustPressed("left_button") || Input.IsActionJustPressed("right_button"))
-        {
-            ChangeState("move");
-            return;
-        }
-
-        if (Input.IsActionJustPressed("jump_button"))
-        {
-            velocity.y -= jump;
-            ChangeState("air");
-            return;
-        }
-
-        if (Input.IsActionJustPressed("interact_button"))
-        {
-            HandleInteract();
-        }
-        if (velocity.y != 0)
-        {
-            ChangeState("air");
-        }
-        GroundMove(0);
-    }
-    private void HandleMoveState()
-    {
-        int direction = Math.Sign(Input.GetActionStrength("right_button") - Input.GetActionStrength("left_button"));
-        if (direction > 0)
-        {
-            sprite.FlipH = false;
-        }
-        else if (direction < 0)
-        {
-            sprite.FlipH = true;
-        }
-        else
-        {
-            ChangeState("idle");
-            return;
-        }
-        if (velocity.y != 0)
-        {
-            ChangeState("air");
-            return;
-        }
-
-        if (Input.IsActionJustPressed("jump_button"))
-        {
-            velocity.y -= jump;
-            ChangeState("air");
-            return;
-        }
-
-        GroundMove(direction);
-
-        if (Input.IsActionJustPressed("interact_button"))
-        {
-            HandleInteract();
-        }
-
-    }
-    private void HandleAirState()
-    {
-        int direction = Math.Sign(Input.GetActionStrength("right_button") - Input.GetActionStrength("left_button"));
-        if (direction > 0)
-        {
-            sprite.FlipH = false;
-        }
-        else if (direction < 0)
-        {
-            sprite.FlipH = true;
-        }
-        AirMove(direction);
-        if (Input.IsActionJustPressed("interact_button"))
-        {
-            HandleInteract();
-        }
-        if (Input.IsActionJustPressed("jump_button"))
-        {
-            if (doubleJump > 0)
-            {
-                velocity.y -= jump;
-                doubleJump--;
-            }
-        }
-    }
-
-    private void HandleInteract()
+    public void HandleInteract()
     {
         Item item;
         if (pickedItem != null)
@@ -195,7 +90,7 @@ public class Player : KinematicBody2D
         }
     }
 
-    private void HandleLadders()
+    public void HandleLadders()
     {
         if (Input.IsActionPressed("jump_button"))
         {
@@ -212,47 +107,17 @@ public class Player : KinematicBody2D
     }
 
 
-    private void ChangeState(string stateName)
+    private void ChangeState(PlayerState playerState)
     {
-        ExitState(currentState);
-        switch (stateName)
-        {
-            case "idle":
-                stateStack.Insert(0, "idle");
-                break;
-            case "move":
-                stateStack.Insert(0, "move");
-                break;
-            case "air":
-                stateStack.Insert(0, "air");
-                break;
-            case "previous":
-                stateStack.RemoveAt(0);
-                currentState = stateStack[0];
-                break;
-            default:
-                GD.Print("Error while changing state");
-                break;
-        }
+        playerState.ExitState(this);
+        stateStack.Insert(0, playerState);
         currentState = stateStack[0];
     }
-    private void ExitState(string state)
+
+    private void moveToPreviousState()
     {
-        switch (state)
-        {
-            case "idle":
-                break;
-            case "move":
-                break;
-            case "air":
-                if (type.Equals("light"))
-                {
-                    doubleJump = 1;
-                }
-                break;
-            default:
-                break;
-        }
+        stateStack.RemoveAt(0);
+        currentState = stateStack[0];
     }
 
     // Handles interacting with items.
--- scripts/utilities/GameManager.cs
+++ scripts/utilities/GameManager.cs
@@ -3,36 +3,36 @@ using System;
 
 public class GameManager : Node
 {
-    private Player player;
-    private MainCamera camera;
-    private DebugOverlay debug;
-    private AudioSource audioSource;
-    public override void _Ready()
-    {
-        player = (Player)GetNode("Player");
-        camera = (MainCamera)GetNode("MainCamera");
-        debug = (DebugOverlay)GetNode("UILayer/DebugOverlay");
+	private Player player;
+	private MainCamera camera;
+	private DebugOverlay debug;
+	private AudioSource audioSource;
+	public override void _Ready()
+	{
+		player = (Player)GetNode("Player");
+		camera = (MainCamera)GetNode("MainCamera");
+		debug = (DebugOverlay)GetNode("UILayer/DebugOverlay");
 
-        debug.AddStat("Fps: ", this, "GetFps", true);
-        debug.AddStat("Nearby Items: ", player, "GetSurroundingItems", true);
-        debug.AddStat("Static Memory Usage: ", this, "GetStaticMemory", true);
-        debug.AddStat("Dynamic Memory usage: ", this, "GetDynamicMemory", true);
-        audioSource = (AudioSourceImplementation)GetNode("AudioManager");
+		debug.AddStat("Fps: ", this, "GetFps", true);
+		debug.AddStat("Nearby Items: ", player, "GetSurroundingItems", true);
+		debug.AddStat("Static Memory Usage: ", this, "GetStaticMemory", true);
+		debug.AddStat("Dynamic Memory usage: ", this, "GetDynamicMemory", true);
+		audioSource = (AudioSourceImplementation)GetNode("AudioManager");
 
-        camera.Initialize(player);
-        player.Initialize();
-    }
+		camera.Initialize(player);
+		player.Initialize();
+	}
 
-    private string GetFps()
-    {
-        return Engine.GetFramesPerSecond().ToString();
-    }
-    private string GetStaticMemory()
-    {
-        return string.Format("{0} Mb", OS.GetStaticMemoryUsage() / 1000000);
-    }
-    private string GetDynamicMemory()
-    {
-        return string.Format("{0} Mb", OS.GetDynamicMemoryUsage() / 1000000);
-    }
+	private string GetFps()
+	{
+		return Engine.GetFramesPerSecond().ToString();
+	}
+	private string GetStaticMemory()
+	{
+		return string.Format("{0} Mb", OS.GetStaticMemoryUsage() / 1000000);
+	}
+	private string GetDynamicMemory()
+	{
+		return string.Format("{0} Mb", OS.GetDynamicMemoryUsage() / 1000000);
+	}
 }
