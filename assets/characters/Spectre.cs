9e8e4f79610c226e4ed13952f50945bec20c24b4
Zapihra <78799958+Zapihra@users.noreply.github.com>
2022-01-30 08:10:32 +0200

    Start of spectre

--- assets/characters/Spectre.cs
+++ assets/characters/Spectre.cs
@@ -3,19 +3,30 @@ using System;
 
 public class Spectre : RigidBody2D
 {
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
+	// Declare member variables here. Examples:
+	// private int a = 2;
+	// private string b = "text";
 
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-        
-    }
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		var animSprite2D = GetNode<AnimatedSprite>("AnimatedSprite");
+		animSprite2D.Playing = true;
+		string[] mobTypes = animSprite2D.Frames.GetAnimationNames();
+		animSprite2D.Animation = mobTypes[1];
+		
+	}
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
 //  public override void _Process(float delta)
 //  {
 //      
 //  }
+	private void _on_VisibilityNotifier2D_screen_exited()
+	{
+		QueueFree();
+	}
 }
+
+
+
