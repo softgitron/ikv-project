903cff2122d28d43c7ec9b0c3036945bab32e24d
Tompparella <58560424+Tompparella@users.noreply.github.com>
2022-01-30 08:58:58 +0200

    swag

--- scripts/player/PlayerMarker.cs
+++ scripts/player/PlayerMarker.cs
@@ -3,17 +3,17 @@ using System;
 
 public class PlayerMarker : Node2D
 {
-    private PositionSync positionSync;
+	private PositionSync positionSync;
 
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-        positionSync = new PositionSync(this, Main.otherPlayer, "player" + Main.otherPlayer.ToString());
-    }
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		positionSync = new PositionSync(this, Main.otherPlayer, "player" + Main.otherPlayer.ToString());
+	}
 
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(float delta)
-    {
+	{
 		positionSync.Update();
-    }
+	}
 }
