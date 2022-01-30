2436da17278511d57c0b9cfc5a5311719134a9b1
Tompparella <58560424+Tompparella@users.noreply.github.com>
2022-01-30 10:50:10 +0200

    Manual Merge

--- scripts/player/states/IdleState.cs
+++ scripts/player/states/IdleState.cs
@@ -2,38 +2,10 @@ using Godot;
 
 public class IdleState : PlayerState
 {
-    public PlayerState ProcessState(Player player)
-    {
-        if (Input.IsActionJustPressed("left_button") || Input.IsActionJustPressed("right_button"))
-        {
-            return new MoveState();
-        }
-
-        if (Input.IsActionJustPressed("jump_button"))
-        {
-            player.velocity.y = -player.jump;
-            return new MoveState();
-        }
-
-        if (Input.IsActionJustPressed("interact_button"))
-        {
-            player.HandleInteract();
-        }
-
-        player.GroundMove(0);
-
-        if (player.velocity.y != 0)
-        {
-            return new AirState();
-        }
-
-        return null;
-    }
     public void HandleAnimationFinished(Player player)
     {
         player.sprite.Play("idle");
     }
-
     public void ExitState(Player player)
     {
 
@@ -73,10 +45,5 @@ public class IdleState : PlayerState
         }
 
         return null;
-    }
-
-    public void ExitState(Player player)
-    {
-
     }
 }
--- scripts/utilities/GameManager.cs
+++ scripts/utilities/GameManager.cs
@@ -3,53 +3,54 @@ using System;
 
 public class GameManager : Node
 {
-    private Player player;
-    private MainCamera camera;
-    private DebugOverlay debug;
-    private AudioSource audioSource;
-    private SpriteFrames lightFrames, darkFrames;
-    public override void _Ready()
-    {
-        lightFrames = (SpriteFrames)ResourceLoader.Load("res://resources/sprites/characters/anim/light_anim.tres");
-        darkFrames = (SpriteFrames)ResourceLoader.Load("res://resources/sprites/characters/anim/dark_anim.tres");
+	private Player player;
+	private MainCamera camera;
+	private DebugOverlay debug;
+	private AudioSource audioSource;
+	private SpriteFrames lightFrames, darkFrames;
+	public override void _Ready()
+	{
+		lightFrames = (SpriteFrames)ResourceLoader.Load("res://resources/sprites/characters/anim/light_anim.tres");
+		darkFrames = (SpriteFrames)ResourceLoader.Load("res://resources/sprites/characters/anim/dark_anim.tres");
 
-        player = (Player)GetNode("Player");
-        camera = (MainCamera)GetNode("MainCamera");
-        debug = (DebugOverlay)GetNode("UILayer/DebugOverlay");
-        audioSource = (AudioSourceImplementation)GetNode("AudioManager");
+		player = (Player)GetNode("Player");
+		camera = (MainCamera)GetNode("MainCamera");
+		debug = (DebugOverlay)GetNode("UILayer/DebugOverlay");
+		audioSource = (AudioSourceImplementation)GetNode("AudioManager");
 
-        debug.AddStat("Fps: ", this, "GetFps", true);
-        debug.AddStat("Nearby Items: ", player, "GetSurroundingItems", true);
-        debug.AddStat("Static Memory Usage: ", this, "GetStaticMemory", true);
-        debug.AddStat("Dynamic Memory usage: ", this, "GetDynamicMemory", true);
+		debug.AddStat("Fps: ", this, "GetFps", true);
+		debug.AddStat("Nearby Items: ", player, "GetSurroundingItems", true);
+		debug.AddStat("Static Memory Usage: ", this, "GetStaticMemory", true);
+		debug.AddStat("Dynamic Memory usage: ", this, "GetDynamicMemory", true);
 
-        camera.Initialize(player);
+		camera.Initialize(player);
 
-        switch (player.type)
-        {
-            case "light":
-                audioSource.FadeInTrack(AudioSourceImplementation.lightPlayerTheme, 0);
-                player.Initialize(lightFrames);
-                break;
-            case "dark":
-                audioSource.FadeInTrack(AudioSourceImplementation.darkPlayerTheme, 0);
-                player.Initialize(darkFrames);
-                break;
-            default:
-                GD.Print("Error while choosing player frames.");
-                break;
-        }
+		switch (player.type)
+		{
+			case "light":
+				audioSource.FadeInTrack(AudioSourceImplementation.lightPlayerTheme, 0);
+				player.Initialize(lightFrames);
+				break;
+			case "dark":
+				audioSource.FadeInTrack(AudioSourceImplementation.darkPlayerTheme, 0);
+				player.Initialize(darkFrames);
+				break;
+			default:
+				GD.Print("Error while choosing player frames.");
+				break;
+		}
+	}
 
-        private string GetFps()
-        {
-            return Engine.GetFramesPerSecond().ToString();
-        }
-        private string GetStaticMemory()
-        {
-            return string.Format("{0} Mb", OS.GetStaticMemoryUsage() / 1000000);
-        }
-        private string GetDynamicMemory()
-        {
-            return string.Format("{0} Mb", OS.GetDynamicMemoryUsage() / 1000000);
-        }
-    }
+	private string GetFps()
+	{
+		return Engine.GetFramesPerSecond().ToString();
+	}
+	private string GetStaticMemory()
+	{
+		return string.Format("{0} Mb", OS.GetStaticMemoryUsage() / 1000000);
+	}
+	private string GetDynamicMemory()
+	{
+		return string.Format("{0} Mb", OS.GetDynamicMemoryUsage() / 1000000);
+	}
+}
